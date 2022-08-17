b1a7c7e63b243c3a0976dae9a27bfedcd7f83d8e
Cem Üstün <ustuncem7@gmail.com>
2022-08-17 22:56:55 +0300

    react-query exercies 1 done

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import axios from "axios";
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  const [interval, setInterval] = useState(3000);
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+    ["deneme"],
+    () => axios.get("http://localhost:4000/superheroes"),
+    {
+      refetchInterval: interval,
+      onError,
+      onSuccess,
+    }
+  );
+
+  function onSuccess(data) {
+    setInterval((prev) => (data.data.length === 4 ? false : prev));
+  }
+
+  function onError(error) {}
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      <button onClick={refetch}>Click me to fetch!</button>
+      {data ? (
+        data?.data.map((hero) => <p key={hero.name}>{hero.name}</p>)
+      ) : isError ? (
+        <span>Error: {error.message}</span>
+      ) : isLoading && !isFetching ? (
+        <span>Not ready ...</span>
+      ) : (
+        <span>Loading...</span>
+      )}
+    </>
+  );
+};
